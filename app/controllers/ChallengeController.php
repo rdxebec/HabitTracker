f8a4937abe644afa223807c539fb77e9af5e8a1d
rdxebec <harshpatel4373@gmail.com>
2026-06-29 15:47:46 +0530

    day9qa

--- app/controllers/ChallengeController.php
+++ app/controllers/ChallengeController.php
@@ -18,23 +18,17 @@ class ChallengeController extends Controller
 
         $challenges = $challengeModel->getAll();
 
+        $dailyChallenge = new DailyChallenge();
+
         $joined = [];
 
         foreach ($challenges as $challenge) {
 
-            $completed =
-                $challengeModel->hasCompleted(
+            $joined[$challenge['id']] =
+                $dailyChallenge->completedToday(
                     $_SESSION['user_id'],
                     $challenge['id']
                 );
-
-            if ($completed) {
-
-                $joined[$challenge['id']] = true;
-            } else {
-
-                $joined[$challenge['id']] = false;
-            }
         }
 
         $this->view(
@@ -45,16 +39,29 @@ class ChallengeController extends Controller
             ]
         );
     }
+    // Check login
 
     public function join()
     {
+
+        // CSRF validation
+        if (
+            !isset($_POST['csrf_token']) ||
+            !hash_equals(
+                $_SESSION['csrf_token'],
+                $_POST['csrf_token']
+            )
+        ) {
+            die('Invalid CSRF Token');
+        }
+
         if (!isset($_SESSION['logged_in'])) {
             header('Location: /habittracker/public/login');
             exit;
         }
 
         $challengeId =
-            (int)($_GET['id'] ?? 0);
+            (int)($_POST['challenge_id'] ?? 0);
 
         $challengeModel =
             new Challenge();
